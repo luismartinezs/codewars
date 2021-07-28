252a7e2f1025bc7e0bdf6b0e104b6c84eabd5c7a
luismartinezs <martinez.s.luis@gmail.com>
2021-07-28 15:07:22 +0200

    Add some helper comments

--- problem-solving-patterns/countUniqueValues.js
+++ problem-solving-patterns/countUniqueValues.js
@@ -1,3 +1,42 @@
 const countUniqueValues = arr => {}
 
 export default countUniqueValues
+
+/*
+11223
+
+
+j = 0
+i = 0
+11223
+--
+same
+
+j = 1
+i = 0
+11223
+--
+same
+
+j = 2
+i = 0
+11223
+--
+i = 1
+12223
+
+j = 3
+i = 1
+12223
+--
+same
+
+j = 4
+i = 1
+12223
+--
+i = 2
+12323
+
+It's reusing the array to add results
+*/
